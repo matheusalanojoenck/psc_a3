d952350cbfc090d4841e95db2914bc9f7846f826
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2023-06-02 16:02:20 -0300

    Update Turma.java

--- src/Turma.java
+++ src/Turma.java
@@ -1,35 +1,21 @@
-import java.util.ArrayList;
+import java.util.List;
 
 public class Turma {
 	private String nome;
 	private Professor professor;
-	private ArrayList<Aluno> alunos;
-	private ArrayList<DiasSemana> dias_semana;
+	private Sala sala;
+	private List<Aluno> alunos;
+	private List<DiasSemana> dias_semana;
 	
-	public String getNome() {
-		return nome;
+	public Turma(){
+		
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
+	public boolean addAluno(Aluno aluno) {
+		if (alunos.size() == sala.getCapacidade()) {
+			return false;
+		}
+		alunos.add(aluno);
+		return true;
 	}
-	public Professor getProfessor() {
-		return professor;
-	}
-	public void setProfessor(Professor professor) {
-		this.professor = professor;
-	}
-	public ArrayList<Aluno> getAlunos() {
-		return alunos;
-	}
-	public void setAlunos(ArrayList<Aluno> alunos) {
-		this.alunos = alunos;
-	}
-	public ArrayList<DiasSemana> getDias_semana() {
-		return dias_semana;
-	}
-	public void setDias_semana(ArrayList<DiasSemana> dias_semana) {
-		this.dias_semana = dias_semana;
-	}
-	
 	
 }
